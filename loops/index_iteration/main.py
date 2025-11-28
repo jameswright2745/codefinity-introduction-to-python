17ac452be3b911cce478f1b68815c810c98ecce2
jameswright2745 <>
2025-11-28 13:57:53 +0000

    Index Iteration

--- loops/index_iteration/main.py
+++ loops/index_iteration/main.py
@@ -1,1 +1,7 @@
 prices = [29.99, 45.50, 12.75, 38.20]
+discount_factor = [0.1, 0.2, 0.15, 0.05]
+
+for i in range(len(prices)): 
+    #print(f"Old price: ${prices[i]}")
+    prices[i] -= prices[i] * discount_factor[i]
+    print(f"Update price for item {i+1}: ${prices[i]:.2f}")
